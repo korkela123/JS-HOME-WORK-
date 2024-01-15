11f618029cb15b6c2da1854625f13e61e94c0425
korkela123 <132228066+korkela123@users.noreply.github.com>
2024-01-15 14:50:52 +0400

    Add files via upload

---
+++ newwwww/app.js
@@ -0,0 +1,118 @@
+     // პენსიის გამოთლა
+
+//function pensia (){
+        //let age = Number(prompt("ჩაწერეთ თქვენი ასაკი რათა გაიგოთ როდის გეკუთვნით პენსია "));
+        //let sapensioasaki = 65;
+        //let answer = age - sapensioasaki
+        //return `თქვენი პენსიის გაცემამდე დარჩნილია კიდევ ${answer} წელი`
+//}
+
+//document.write(pensia())
+
+     // კალკულატორი
+
+//function calculator (){
+
+     //let num1 = Number(prompt("ჩაწერეთ პირველი რიცხვი"))
+     //let action = prompt("ჩაწერეთ სასურველი მოქმედება ( * , / , - , + )")
+     //let num2 = Number(prompt("ჩაწერეთ მეორე რიცხვი"))
+     //switch (action){
+        //case '*':
+            //document.write(num1 * num2)
+            //break;
+
+        //case '-':
+            //document.write(num1 - num2)
+        //break;
+
+        //case '/':
+             //document.write(num1 / num2)
+        //break;
+
+        //case '+':
+            //document.write(num1 + num2)
+        //break;
+        //default:
+            //document.write("ეს ოპერაცია არარის ხელმისაწდომი")
+        //break
+     //}
+
+
+//}
+
+//calculator()
+
+
+      //პაროლის შეყვანა
+
+//function checkPasswordStrength (){
+    //let password = prompt("შეიყვანეთ პაროლი")
+
+    //if(password.length >= 8 && password.length <= 16  ){
+        //document.write("პაროლი მიღებულია")
+    //}else if (password.length < 8){
+        //document.write("პაროლი უნდა აღემატებოდეს მინიმუმ 8 სიმბოლოს ")
+    //}else if (password.length > 16) {
+        //document.write("პაროლი არ უნდა აღემატებოდეს 16 სიმბოლოს ")
+    //}else if (password.length === 0 ) {
+        //document.write("გთხოვთ შეავსოთ prompt ველი ")
+    //}else{
+        //document.write("EROR")
+    //}
+//}
+
+      //checkPasswordStrength()
+
+    
+      // ყვავილის არჩევა 
+
+
+
+//function flowers () {
+     
+     //let flowersbox = prompt("ჩაწერეთ თქვენთვის სასურველი ყვავილის სახელი 'ქართულად'")
+     //switch(flowersbox){
+        //case "ყაყაჩო":
+            //console.log("ყაყაჩო წარმატებით არის ნაპოვნი ")
+        //break;
+        //case "ვარდი":
+            //console.log("ვარდი წარმატებით არის ნაპოვნი ")
+        //break;
+        //case "ია":
+            //console.log("ია წარმატებით არის ნაპოვნი ")
+        //break;
+        //case "გვირილა":
+            //console.log("გვირილა წარმატებით არის ნაპოვნი ")
+        //break;
+        //default:
+            //console.log("სამწუხაროდ მარაგში არ მოიპოვება  მზგავსი სახეობის ყვავილი სხვა სცადეთ  ")
+        //break;
+    //}
+//}
+
+//flowers()
+    
+     // უდიდესი რიცხვი 
+
+//function biggest () {
+
+    //let n1 = prompt("ჩაწერეთ პირველი რიცხვი")
+    //let n2 = prompt("ჩაწერეთ მეორე რიცხვი")
+    //let n3 = prompt("ჩაწერეთ მესამე რიცხვი")
+
+     //if( n1 > n2 && n1 > n3){
+        //console.log(`ამ სამ რიცხვს შორის უდიდესია ${n1}`)
+     //}else if (n2 > n1 && n2 > n3  ){
+        //console.log(`ამ სამ რიცხვს შორის უდიდესია ${n2}`)
+     //}else if (n3 > n1 && n3 > n2){
+        //console.log(`ამ სამ რიცხვს შორის უდიდესია ${n3}`)
+     //}else if (n1 == n2 && n2 == n3 && n3 == n1 ){
+        //console.log (" ეს რიცხვები არის თანაბარი ")
+     //}else{
+        //console.log("EROR")
+     //}
+//}
+
+//biggest()
+
+document.write("ყველაფერი დაკომენტარებულია ")
